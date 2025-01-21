75236b317bdcbd6f85fa782053c7893440edaf69
xiaoyu1998 <1634054493@qq.com>
2025-01-21 10:59:52 +0800

    add baseSepolia and monadDevnet

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -22,20 +22,34 @@ const getEnvAccount = () => {
 const config: HardhatUserConfig = {
   solidity: "0.8.27",
   networks:{
-      localnet: {
-          url: getRpcUrl("localnet"),
-          chainId: 1998,
-          accounts: getEnvAccount(),
-          blockGasLimit: 20_000_000,
-          gas: 20_000_000,
-      },
-      arbitrumSepolia: {
-          url: getRpcUrl("arbitrumSepolia"),
-          chainId: 421614,
-          accounts: getEnvAccount(),
-          blockGasLimit: 20_000_000,
-          gas: 20_000_000,
-      },
+        localnet: {
+            url: getRpcUrl("localnet"),
+            chainId: 1998,
+            accounts: getEnvAccount(),
+            blockGasLimit: 20_000_000,
+            gas: 20_000_000,
+        },
+        arbitrumSepolia: {
+            url: getRpcUrl("arbitrumSepolia"),
+            chainId: 421614,
+            accounts: getEnvAccount(),
+            blockGasLimit: 20_000_000,
+            gas: 20_000_000,
+        },
+        baseSepolia: {
+            url: getRpcUrl("baseSepolia"),
+            chainId: 84532,
+            accounts: getEnvAccount(),
+            blockGasLimit: 20_000_000,
+            gas: 20_000_000,
+        },
+        monadDevnet: {
+            url: getRpcUrl("monadDevnet"),
+            chainId: 20143,
+            accounts: getEnvAccount(),
+            blockGasLimit: 20_000_000,
+            gas: 20_000_000,
+        },
   }
 };
 
